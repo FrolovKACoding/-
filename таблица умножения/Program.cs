c766d76c3d71725fd26342a9ae8d26e0b1540d69
user <user@WIN-VRLJJQL8MTP>
2023-02-13 22:21:52 +0500

    Добавьте файлы проекта.

---
+++ таблица умножения/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace таблица_умножения
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=1, b;
+            for (; a < 10; ++a, Console.Write('\n'))
+                for (b = 1; b < 10; ++b)
+                    Console.Write("{0,3}", a * b);
+            Console.ReadKey();
+        }
+    }
+                
+}
